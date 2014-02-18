f2f9717b454a2ae497392fedec1ba6d229bd2839
BoppreH <boppreh@gmail.com>
2014-02-18 00:20:10 -0300

    Add explanation at the top

--- flappy_bot.js
+++ flappy_bot.js
@@ -1,3 +1,21 @@
+/***
+
+Adds two checkboxes the top bar: "Autojump" and "Jump at mouse".
+
+"Autojump" makes the bird automatically jump to pass the gaps on the pipes.
+"Jump at mouse" changes the autojump behavior so it jumps at the mouse position.
+
+All AI is done by reading the canvas, and the jump is done by simulating a mouse press.
+Since it read the pixels value to understand the game, it may have issues on:
+
+a) Player's nicknames (shouldn't be an issue after the 3rd pipe, where there are few people).
+b) "Performance" mode, where the other birds have full alpha.
+c) The end of the game, where there are no pipes remaining.
+
+So, for best results disable "performance mode", don't use a nickname and give it a few tries
+to get past the mass of the players.
+
+***/
 (function () {
 
 // Max value for each R, G and B value for a pixel to be considered part of the line of a pipe.
